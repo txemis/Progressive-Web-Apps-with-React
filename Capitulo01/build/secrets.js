ba2963102dd7aba9c9fb69af6f37c16bf9fcc7eb
txemis <jmltil@gmx.com>
2020-04-21 17:38:04 +0000

    otra mas

--- Capitulo01/build/secrets.js
+++
@@ -1,3 +0,0 @@
-window.apiKey = "AIzaSyDiKWwo0DhHa5GydmW7EzaLJyoFYc0sC7c"
-messagingSenderId = "82686351080"
-appId = "1:82686351080:web:2bf281b1d114aaeab39b5d"
--- Capitulo01/src/components/App.js
+++ Capitulo01/src/components/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
+import { Route } from 'react-router-dom';
 import LoginContainer from './LoginContainer';
+import ChatContainer from './ChatContainer';
 import './app.css';
 
 class App extends Component {
@@ -16,8 +18,9 @@ class App extends Component {
 
     render() {
         return (
-            <div id="container" className="inner-container">
-                <LoginContainer />
+            <div id="container">
+                <Route path="/login" component={LoginContainer} />
+                <Route exact path="/" component={ChatContainer} />
             </div>
         );
     }
