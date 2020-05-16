9c6c0b51d24b2d0064a31c5af8efcaad44620e0b
txemis <jmltil@gmx.com>
2020-05-16 12:05:00 +0000

    otro

---
+++ Capitulo004/src/index.js
@@ -0,0 +1,10 @@
+//var React = require('react');
+//var ReactDOM = require('react-dom');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+
+//ReactDOM.render(React.createElement('h1', null, 'Hello from react!'), document.getElementById('root'));
+//ReactDOM.render(<h1>Hello from ES6!</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ Capitulo004/webpack.config.js
@@ -0,0 +1,56 @@
+module.exports = {
+    entry: __dirname + "/src/index.js",
+    output: {
+        path: __dirname + "/public",
+        filename: "bundle.js",
+        publicPath: "/",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                        loader: 'babel-loader' ,
+                        options: {
+                            presets: ['es2015','react'],
+                            plugins: [/**'react-hot-loader/babel',**/'transform-class-properties']
+                        }
+                }
+                
+            },
+            /**
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            }
+            **/
+        ]
+        
+    },
+
+
+    /**
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015','react'],
+                    plugins: ['transform-class-properties']
+                }
+            },
+        ]
+    },**/
+
+    //Podría haber un módulo de Dev server para facilitar el desarrollo
+    //Lo he evitado aquí
+
+};
+
+
