3a7d43fc0232f8b2594d604ae77686f48edc6489
txemis <jmltil@gmx.com>
2020-05-16 18:05:35 +0000

    otro

--- Capitulo004/webpack.config.prod.js
+++ Capitulo004/webpack.config.prod.js
@@ -1,3 +1,6 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: __dirname + "/src/index.js",
     output: {
@@ -15,7 +18,7 @@ module.exports = {
                         loader: 'babel-loader' ,
                         options: {
                             presets: ['es2015','react'],
-                            plugins: [/**'react-hot-loader/babel',**/'transform-class-properties']
+                            plugins: [/**'react-hot-loader/babel',**/'transform-class-properties'],
                         }
                 }
                 
@@ -32,7 +35,29 @@ module.exports = {
         ]
         
     },
-
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: __dirname + "/public/index.html",
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true,
+            },
+        })
+    ],
 
     /**
     module: {
